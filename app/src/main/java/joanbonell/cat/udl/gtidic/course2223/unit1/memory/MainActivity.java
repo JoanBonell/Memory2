a7081f73c7060f901c853d1ef90a44fd56f633f9
JoanBonell <100761163+JoanBonell@users.noreply.github.com>
2023-02-13 16:13:50 +0100

    first commit

---
+++ app/src/main/java/joanbonell/cat/udl/gtidic/course2223/unit1/memory/MainActivity.java
@@ -0,0 +1,29 @@
+package joanbonell.cat.udl.gtidic.course2223.unit1.memory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Uri uri = Uri.parse("https://www.campusigualada.udl.cat");
+
+
+        Button startGame = findViewById(R.id.startGame);
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
